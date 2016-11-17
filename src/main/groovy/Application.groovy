461ff4f93eb014e44a9e258a12045b251da95dcf
Aruis <lovearuis@gmail.com>
2016-11-17 19:26:09 +0800

    Initial commit

---
+++ src/main/groovy/Application.groovy
@@ -0,0 +1,51 @@
+/**
+ * Created by aruis on 16-11-17.
+ */
+class Application {
+    static def list = []
+
+    public static void main(String[] args) {
+
+        def file = System.getProperty('file')
+
+        if (file == null)
+            file = 'D:\\file.xls'
+
+        new ExcelBuilder(file).eachLine([labels: true]) {
+
+            def map = [id: 用户名, no: 份数.toInteger(), pid: 推荐人]
+
+            list << map
+        }
+
+        def result = ''
+        list.each {
+            def _children = getChildren(it)
+            it.total = 0
+            def _childrenStr = ''
+            _children.each { child ->
+                it.total += child.no
+                _childrenStr += child.id + ','
+            }
+
+            result += "$it.id,$it.total,$_childrenStr"
+            result += '\n'
+        }
+
+        new File('D\\result.csv') << result
+    }
+
+
+    static List getChildren(item) {
+        def result = []
+        def children = list.findAll { it.pid == item.id }
+
+        result << children
+        children.each {
+            result << getChildren(it)
+        }
+
+        return result.flatten()
+    }
+
+}
